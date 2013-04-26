0a6bd10a46fbc3b64f2943e0ab7ea6029f195562
Dylan Smith <foliomedia@gmail.com>
2013-04-25 17:20:51 -0700

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CommentsDashboard::Application.config.secret_token = '955dc36922f80221041c8f5327e30afa46bd39899637b6246ae4c4b4761aad8ca2055ac944d36ce5744cb0f77bb99027161da4e1588a3ff9b592f837f3b69f45'
